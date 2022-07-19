6f0b86848c211ddef6411f429af4ffc9e0727135
Gabor Horvath-Ulenius <mobahug@gmail.com>
2022-07-20 00:52:30 +0300

    added bugfixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,16 +57,16 @@ skillsHeader.forEach((el) => {
 const tabs = document.querySelectorAll('[data-target]'),
 	tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab => {
-	tab.addEventListener('click', () => {
+tabs.forEach(tab =>{
+	tab.addEventListener('click', () =>{
 		const target = document.querySelector(tab.dataset.target)
 
-		tabContents.forEach(tabContent => {
+		tabContents.forEach(tabContent =>{
 			tabContent.classList.remove('qualification__active')
 		})
 		target.classList.add('qualification__active')
 
-		tab.forEach(tab => {
+		tabs.forEach(tab =>{
 			tab.classList.remove('qualification__active')
 		})
 		tab.classList.add('qualification__active')
